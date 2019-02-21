b76d462a9f17076f52e63dc640361ece96c0ebfe
Faisal Alvi <faisalalvi33@gmail.com>
2019-02-21 12:38:57 +0530

    header changed with new banner static image

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,11 +5,12 @@ import styles from './hero.module.css'
 
 export default ({ data }) => (
   <div className={styles.hero}>
-    <Img className={styles.heroImage} alt={data.name} sizes={data.heroImage.sizes} />
-    <div className={styles.heroDetails}>
+    {/* <Img className={styles.heroImage} alt={data.name} sizes={data.heroImage.sizes} /> */}
+    <img alt={data.name} src="https://fencecore.com/wp-content/uploads/2018/12/blog-banner-2-2.jpg" />
+    {/* <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>{data.name}</h3>
       <p className={styles.heroTitle}>{data.title}</p>
       <p>{data.shortBio.shortBio}</p>
-    </div>
+    </div> */}
   </div>
 )
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 import base from './base.css'
 import Container from '../components/container'
 import Navigation from '../components/navigation'
+import '../css/style.css'
 
 class Template extends React.Component {
   render() {
@@ -16,7 +17,12 @@ class Template extends React.Component {
 
     return (
       <Container>
-        <Navigation />
+        <div className="header">
+          <div className="logo-container">
+            <img src="//images.ctfassets.net/4ubcz43h3w7v/1r08zza6LeopKoRtFtlfoG/f9522f072346ad116905b30ec6c243db/cipherblog.png" />
+          </div>
+          <Navigation />
+        </div>
         {children()}
       </Container>
     )
